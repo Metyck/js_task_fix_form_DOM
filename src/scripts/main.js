15ef919ea1fcc06049cdf36fdb0b49e16f181abe
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-19 18:32:32 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const fieldsNodeList = document.querySelectorAll('.field');
+const fields = Array.from(fieldsNodeList);
+
+function setFormsInfo(list) {
+  for (const field of list) {
+    if (field.children.length === 0) {
+      continue;
+    }
+
+    const input = field.children[0];
+    const inputId = input.getAttribute('id');
+
+    input.placeholder = input.getAttribute('name');
+
+    const label = document.createElement('label');
+
+    label.setAttribute('for', inputId);
+    label.textContent = input.getAttribute('name');
+    label.classList = 'field-label';
+
+    input.before(label);
+  }
+}
+
+setFormsInfo(fields);
